f07f6f96b0e7b392619d854b2606a388a3fda7f4
Jackson Kelley <jackson.kelley.g@gmail.com>
2019-09-26 23:51:01 -0700

    added image to constructor to get data from

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-function Hotel(name, rooms, booked) {
+function Hotel(name, image, rooms, booked,) {
     this.name = name;
+    this.image = image;
     this.rooms = rooms;
     this.booked = booked;
     this.availableRooms = function() {
         return this.rooms - this.booked;
     };
 }
-let quayHotel = new Hotel('Quay Hotel', 40, 25);
-let portlandHotel = new Hotel('checkSpotPDX');
+let hotels = [
+    new Hotel('Quay Hotel', '../images/hotel1.jpg', 40, 25),
+    new Hotel('CheckInPDX', '../images/hotel2.jpeg', 15, 4)
+];
 
+//MADE HOTELS
+let hotelTitle = document.getElementsByClassName('hotel');
 
-const titleAvailable = document.getElementById('h1');
-titleAvailable.textContent = quayHotel.name + ' has ' + quayHotel.availableRooms() + ' rooms available';
-
-function changeImage() {
-    document.getElementById('img1').style.zIndex = '2';
+for(let i = 0; i < hotels.length; i++) {
+    hotelTitle[i].textContent = hotels[i].name + ' has ' + hotels[i].availableRooms() + ' rooms available';    
 }
+//CHANGED TEXT OF DIV H1
+
+function image(file) {
+    const img1 = document.getElementById('img');
+    img1.style.height = '300px';
+    img1.style.width = '500px';
+    hotelTitle[0].textContent = '';
+    hotelTitle[1].textContent = '';
+    img1.src = file.image;
+    console.log(img1.src);
+}
+//FUNCTION ADDS IMAGE
